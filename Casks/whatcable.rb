97b70737b587e533de30d87b4c9abb543487c13c
Darryl Morley <darrylmorley.uk@gmail.com>
2026-05-15 23:15:41 +0100

    WhatCable 0.10.11

--- Casks/whatcable.rb
+++ Casks/whatcable.rb
@@ -1,6 +1,6 @@
 cask "whatcable" do
-  version "0.10.10"
-  sha256 "fb4b2e4708dc0cc2e7cfb2ca046dc168edff74d4d17a9d7a2d51ff036611694c"
+  version "0.10.11"
+  sha256 "0d93d7c65180f4246cc4da5e70314b535417344b565557d309568a0fe9c1e616"
 
   url "https://github.com/darrylmorley/whatcable/releases/download/v#{version}/WhatCable.zip",
       verified: "github.com/darrylmorley/whatcable/"
